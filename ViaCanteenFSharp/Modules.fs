6393418027bf6c78ca129f5540909f1766b97965
Filip Skukan <skukan159@gmail.com>
2019-03-19 11:10:18 +0100

    Added comments for requirements in the modules file

--- ViaCanteenFSharp/Modules.fs
+++ ViaCanteenFSharp/Modules.fs
@@ -1,2 +1,14 @@
 ﻿module Modules
 
+(*
+Design a type to represent different types of food in the canteen.
+Implement price calculation
+Have at least:
+3 kinds of salad
+3 kinds of sandwitch
+3 kinds of cake
+Hints: You will need to define the data type for the food amd size(union)
+and the content (for instance a bag/wrap) with the size (record).
+You will also need to define
+the price calculation function that uses pattern matching.
+*)
