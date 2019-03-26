20f4d058b0b7e5ab99140f6be56e5a88dfefd821
Filip Skukan <skukan159@gmail.com>
2019-03-26 09:30:44 +0100

    Added josephs requirements

--- ViaCanteenFSharp/Program.fs
+++ ViaCanteenFSharp/Program.fs
@@ -1,16 +1,16 @@
 ﻿// Learn more about F# at http://fsharp.org
 // See the 'F# Tutorial' project for more help.
 
-open CanteenItems
+open JosephsSpecialCanteenItems
 open System
 
 [<EntryPoint>]
 let main argv = 
 
 
-    printfn "Large chicken salad costs: %A DKK" (CalculateCanteenItemPrice largeChickenSalad)
-    printfn "Medium chocolate cake costs: %A DKK" (CalculateCanteenItemPrice mediumChocolateCake)
-    printfn "Small poultry sandwich costs: %A DKK" (CalculateCanteenItemPrice smallPoultrySandwich)
+    printfn "Large chicken salad costs: %A DKK" (CalculateCanteenItemPrice Chicken Large)
+    printfn "Medium chocolate cake costs: %A DKK" (CalculateCanteenItemPrice Chocolate Medium)
+    printfn "Small poultry sandwich costs: %A DKK" (CalculateCanteenItemPrice Poultry Small)
 
     Console.ReadLine()
     0 // return an integer exit code
